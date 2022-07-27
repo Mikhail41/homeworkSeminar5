11bdb92e55761753016fa898071d8109782f2897
Mikhail <mikhail-zmyslin@yandex.ru>
2022-07-28 00:04:06 +0300

    добавил решение второй задачи

---
+++ task002seminar5/Program.cs
@@ -0,0 +1,50 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.Clear();
+Console.WriteLine("Эта программа задаёт одномерный массив, заполненный случайными числами и находит сумму элементов, стоящих на нечётных позициях.");
+Console.WriteLine();
+
+int n = new Random().Next(3, 12);
+Console.WriteLine($"Размерность массива =: " + n);
+
+int[] fillArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-99, 100);
+    }
+    return arr;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+}
+int[] arr = new int[n];
+
+fillArr(arr);
+PrintArray(arr);
+Console.WriteLine();
+int sumElements(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 1)
+        {
+            sum = sum + arr[i];
+        }
+
+    }
+    return sum;
+}
+
+int quantity = sumElements(arr);
+Console.WriteLine();
+Console.WriteLine($"Сумма нечетных элементов =: {quantity}");
+Console.WriteLine();
